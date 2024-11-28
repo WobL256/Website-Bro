7a32c41bb69712c83db34d3c83add2d42b3b7743
WobL256 <voicuandrei20@gmail.com>
2024-11-28 21:47:12 +0200

    fixed posts server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3100;
 const POSTS_FILE = 'posts.json';
 
 app.use(cors());
@@ -52,6 +52,6 @@ app.post('/comments', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
